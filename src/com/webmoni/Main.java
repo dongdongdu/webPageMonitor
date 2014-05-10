55ea92daa7c20c298203b86eafd8087d16935ab5
杜栋栋 <du.dongdong@outlook.com>
2014-05-10 21:14:02 +0800

    fix

--- src/com/webmoni/Main.java
+++ src/com/webmoni/Main.java
@@ -2,14 +2,15 @@ package com.webmoni;
 
 import java.text.ParseException;
 
+import com.pkucareer.intern.SubscribeInterns;
 import com.pkucareer.intern.SubscribeJobs;
 
 public class Main {
 
     public static void main(String[] args) throws ParseException {
 
-        // SubscribeInterns subscribeInterns = new SubscribeInterns();
-        // subscribeInterns.ProcessSubscription();
+        SubscribeInterns subscribeInterns = new SubscribeInterns();
+        subscribeInterns.ProcessSubscription();
 
         SubscribeJobs subscribeJobs = new SubscribeJobs();
         subscribeJobs.ProcessSubscription();
