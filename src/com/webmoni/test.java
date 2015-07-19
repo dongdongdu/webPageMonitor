d06ae58b5d7c125d0678e84c38507c417d7e3faf
杜栋栋 <du.dongdong@outlook.com>
2015-07-19 14:39:03 +0800

    test

--- src/com/webmoni/test.java
+++ src/com/webmoni/test.java
@@ -1,17 +1,14 @@
 package com.webmoni;
 
-import com.webmoni.util.Utils;
-
 public class test {
 
     public static void main(String[] args) {
 
-        String a1 = "abc";
-        String a2 = "呵呵呵";
-        String a3 = "abc哈哈哈";
-        Utils.writeToLog(a1);
-        Utils.writeToLog(a2);
-        Utils.writeToLog(a3);
+        String aa = "313121/jfaksdjf";
+
+        String[] strings = aa.split("[\\\\/]", 2);
+
+        System.out.println();
     }
 
 }
